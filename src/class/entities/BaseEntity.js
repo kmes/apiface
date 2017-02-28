f47cff288a58800e00d6574ec0fe9d73b6185ec4
Francesco Semeraro <francesco.semeraro@mazzmedia.com>
2017-02-28 15:25:37 +0100

    fix BaseEntity

--- src/class/entities/BaseEntity.js
+++ src/class/entities/BaseEntity.js
@@ -104,6 +104,7 @@ export default class BaseEntity {
     }
     set( field, value ) {
         let data = this.getData();
+        if( data === null ) this.setData({});
         if( typeof data !== 'object' ) return false;
         data[ field ] = value;
 
